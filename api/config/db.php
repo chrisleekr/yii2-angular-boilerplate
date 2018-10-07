905a0b0b70b0c74eaac7431c70f852abeeac0b35
Chris Lee <git@chrislee.kr>
2018-10-07 18:39:37 +1100

    Fixed travis ci for db connection and chrome for Karma

--- api/config/db.php
+++ api/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('MYSQL_DATABASE'),
+    'dsn' => 'mysql:host='.getenv('MYSQL_HOST').';port='.getenv('MYSQL_PORT').';dbname='.getenv('MYSQL_DATABASE'),
     'username' => getenv('MYSQL_USERNAME'),
     'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8',
--- api/config/test_db.php
+++ api/config/test_db.php
@@ -1,6 +1,6 @@
 <?php
-$db = require(__DIR__ . '/db.php');
+$db = require(__DIR__.'/db.php');
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('MYSQL_DATABASE');
+$db['dsn'] = 'mysql:host='.getenv('MYSQL_HOST').';port='.getenv('MYSQL_PORT').';dbname='.getenv('MYSQL_DATABASE');
 
 return $db;
