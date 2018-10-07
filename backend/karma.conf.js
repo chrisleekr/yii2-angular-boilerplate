70ba2146c3667b0ab83d75a006da4093bca78e33
Chris Lee <git@chrislee.kr>
2018-10-07 19:19:55 +1100

    Fixed the issue with loading env and Karma chrome launching error

--- backend/karma.conf.js
+++ backend/karma.conf.js
@@ -27,7 +27,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    // you can define custom flags
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
