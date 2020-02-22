cbde72d1f3249677d2561ef45fc4aee4e040be84
Chris Lee <git@chrislee.kr>
2020-02-22 16:56:39 +1100

    Fixed tests

--- backend/karma.conf.js
+++ backend/karma.conf.js
@@ -25,8 +25,15 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     singleRun: false,
+    // you can define custom flags
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     restartOnFileChange: true
   });
 };
